e8fb7e8265cb85b799ca7bed9088323eeef1b575
bal16 <ahmadhamidbalya@gmail.com>
2024-07-11 17:24:04 +0700

    fix: change darkmode trigger to class

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,7 @@ export default {
   content: [
     "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php", "./storage/framework/views/*.php", "./resources/views/**/*.blade.php", "./resources/js/**/*.tsx"
   ],
-  darkMode: [
-    "selector", '[data-mode="dark"]'
-  ],
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
