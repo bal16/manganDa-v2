1ea6a3c39795e6e32db60b3468b38e3536bb57ca
bal16 <ahmadhamidbalya@gmail.com>
2024-07-12 07:14:58 +0700

    ui: change bg color

--- resources/js/Components/layouts/Header.tsx
+++ resources/js/Components/layouts/Header.tsx
@@ -1,13 +1,12 @@
-import { ReactNode } from "react"
+import { ReactNode } from "react";
 
-const Header = ({children}:{children:ReactNode}) => {
-  return (
-            <header className="flex justify-between items-center h-12 p-2 border-b-[0.1px] border-primary-950 bg-primary-50">
-                <span className="text-xl">manganda</span>
-                {children}
-            </header>
+const Header = ({ children }: { children?: ReactNode }) => {
+    return (
+        <header className="flex justify-between items-center h-12 p-2 border-b-[0.1px] border-primary-950 bg-transparent">
+            <span className="text-xl">manganda</span>
+            {children}
+        </header>
+    );
+};
 
-  )
-}
-
-export default Header
+export default Header;
