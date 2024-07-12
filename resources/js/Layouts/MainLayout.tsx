645b782d652f7a32021f46986edc6c2379b41406
bal16 <ahmadhamidbalya@gmail.com>
2024-07-12 07:12:39 +0700

    type: indexing prop types

--- resources/js/Layouts/MainLayout.tsx
+++ resources/js/Layouts/MainLayout.tsx
@@ -1,32 +1,21 @@
 import Navbar from "@/Components/layouts/Navbar";
 import NavbarRes from "@/Components/layouts/NavbarRes";
 import Sidebar from "@/Components/layouts/Sidebar";
-import { PageProps } from "@/types";
+import { MainLayoutProps } from "@/types";
 import { Head } from "@inertiajs/react";
-import { ReactNode } from "react";
 
-type MainLayoutProps = {
-    children: ReactNode;
-    auth?: PageProps;
-    title: string;
-};
-
-const MainLayout = ({ children,  title }: MainLayoutProps) => {
+const MainLayout = ({ children, title }: MainLayoutProps) => {
     return (
         <div className="bg-primary-50">
-
-        <div className="container flex w-full h-full mx-auto bg-primary-50 max-w-7xl">
-            <Head title={title} />
-            <Navbar  />
-            <main className="w-full min-h-screen mx-auto text-center border-primary-950 bg-primary-50 text-primary-950">
-                <section className="min-h-screen">
-                {children}
-
-                </section>
-            <NavbarRes />
-            </main>
-            <Sidebar  /> {/* assign or fetch merchant */}
-        </div>
+            <div className="container flex w-full h-full mx-auto bg-primary-50 max-w-7xl">
+                <Head title={title} />
+                <Navbar />
+                <main className="w-full min-h-screen mx-auto text-center border-primary-950 bg-primary-50 text-primary-950  md:shadow-primary-950 md:shadow-inner">
+                    <section className="min-h-screen">{children}</section>
+                    <NavbarRes />
+                </main>
+                <Sidebar /> {/* assign or fetch merchant */}
+            </div>
         </div>
     );
 };
