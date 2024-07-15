b4faf07d8a81d7cdb3c1f7cd976d4123be870f9b
bal16 <ahmadhamidbalya@gmail.com>
2024-07-15 11:05:44 +0700

    feat (pagination): add pagination types

--- resources/js/types/entity.d.ts
+++ resources/js/types/entity.d.ts
@@ -100,3 +100,35 @@ export interface Like {
     post: Post;
     created_at: string;
 }
+
+export interface PaginatedDatas {
+    current_page: number,
+    data?: Post[]|Store[]|Report[]|Comment[]|,
+    first_page_url?: string,
+    from: number,
+    last_page: number,
+    last_page_url: string,
+    links: [
+      {
+        url?: string,
+        label: string,
+        active: boolean
+      },
+      {
+        url: string,
+        label: string|number,
+        active: boolean
+      },
+      {
+        url?: string,
+        label: string,
+        active: boolean
+      }
+    ],
+    next_page_url?: string,
+    path: string,
+    per_page: number,
+    prev_page_url?: string,
+    to: number,
+    total: number
+}
