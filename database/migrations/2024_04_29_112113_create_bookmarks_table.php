aa45865325872af5eeb7e80aff2a6f72c7908881
bal16 <ahmadhamidbalya@gmail.com>
2024-07-15 20:47:10 +0700

    🐞 fix(table): change data type

--- database/migrations/2024_04_29_112113_create_bookmarks_table.php
+++ database/migrations/2024_04_29_112113_create_bookmarks_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('bookmarks', function (Blueprint $table) {
-            $table->string('id')->primary();
+            $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->foreignId('post_id')->constrained('posts')->onDelete('cascade');
             // $table->unsignedBigInteger('user_id');
